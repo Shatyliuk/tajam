391c7c90703f679638efc7938a810a8efcfd3df8
Shatyliuk <shatyliuk@gmail.com>
2018-01-15 12:30:57 +0200

    add media quaries

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -4,4 +4,10 @@ $(document).ready(function () {
         items: 1,
         nav: true
     });
+
+    $('.menuButton').click(function () {
+        $(this).toggleClass('open');
+        $('.menu').toggleClass('menu-open');
+        $('.menu__item').toggleClass('menu__item-open')
+    });
 });
